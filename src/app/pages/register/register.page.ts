3ece7fd04ed78e31c40ad7f87aaca2c6cb151060
FoxJayOur <dimacjoshryan@gmail.com>
2023-03-06 13:59:15 +0800

    register fix

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -19,7 +20,7 @@ export class RegisterPage implements OnInit {
   program: string
   progName: string
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router, private alertController: AlertController) { }
 
   ngOnInit() {
   }
@@ -45,9 +46,22 @@ export class RegisterPage implements OnInit {
       console.log(res)
     }, error =>{
       console.log(error)
+      this.presentAlert('Register Failed', 'Wrong information filled out')
     })
 
     console.log(user)
+  }
+  async presentAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: header,
+      message: message,
+      buttons: ['OK']
+    });
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    
   }
 
 
